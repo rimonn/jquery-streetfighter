bf82aa6a2f64399e47350340ba7439a218ae2757
Nadav Rimon <rimonn@gmail.com>
2016-10-08 18:29:33 +0300

    Animation Walkthrough

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 
 $(document).ready(function() {
 
-    //ryu container: mouseenter & mouseleave
+    //handle ryu according to mouse events
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
@@ -13,6 +13,30 @@ $(document).ready(function() {
     .mouseleave(function() {
         $('.ryu-ready').hide();
         $('.ryu-still').show();
-    });
+    })
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
 
+        $('.hadouken').finish().show()
+            .animate(
+                {'left': '1020px'},
+                500,
+                function() {
+                    $('.hadouken').hide();
+                    $('.hadouken').css('left', '588px');
+                }
+            );
+    })
+    .mouseup(function() {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
